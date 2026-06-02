a91bb38ece3b9996a3915b88dab87d6e0ac114ae
Raman Mishra <ramanmi293@gmail.com>
2026-06-03 04:41:52 +0530

    Added email password login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,35 +3,63 @@
 import { useState } from "react";
 
 export default function LoginPage() {
-  const [email, setEmail] = useState("");
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
 
-  function handleLogin() {
-    localStorage.setItem("token", "rakshanova-token");
-    localStorage.setItem("email", email);
+function handleLogin() {
+const user = JSON.parse(
+localStorage.getItem("user") || "{}"
+);
 
-    window.location.href = "/dashboard";
-  }
 
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-black text-white">
-      <div className="w-96 rounded-xl bg-slate-900 p-6">
-        <h1 className="mb-4 text-2xl font-bold">Login</h1>
-
-        <input
-          type="email"
-          placeholder="Email"
-          className="mb-3 w-full rounded bg-slate-800 p-3"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-
-        <button
-          onClick={handleLogin}
-          className="w-full rounded bg-cyan-500 p-3 font-bold text-black"
-        >
-          Login
-        </button>
-      </div>
-    </div>
+if (
+  email === user.email &&
+  password === user.password
+) {
+  localStorage.setItem(
+    "token",
+    "rakshanova-token"
   );
+
+  localStorage.setItem("email", email);
+
+  window.location.href = "/dashboard";
+} else {
+  alert("Invalid Email or Password");
+}
+
+
+}
+
+return ( <div className="min-h-screen flex items-center justify-center bg-black text-white"> <div className="w-96 rounded-xl bg-slate-900 p-6"> <h1 className="mb-4 text-2xl font-bold">
+Login </h1>
+
+
+    <input
+      type="email"
+      placeholder="Email"
+      className="mb-3 w-full rounded bg-slate-800 p-3"
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
+    />
+
+    <input
+      type="password"
+      placeholder="Password"
+      className="mb-3 w-full rounded bg-slate-800 p-3"
+      value={password}
+      onChange={(e) => setPassword(e.target.value)}
+    />
+
+    <button
+      onClick={handleLogin}
+      className="w-full rounded bg-cyan-500 p-3 font-bold text-black"
+    >
+      Login
+    </button>
+  </div>
+</div>
+
+
+);
 }
